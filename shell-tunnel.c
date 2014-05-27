10990cc3f58b827a103d25b1c6d1dedd9876aaee
Andrey Gelman <andrey.gelman@gmail.com>
2014-05-27 22:38:50 +0300

    Provide console <--> socket proxy
    Replace client test code with console <--> socket proxy.
    Server side, at the moment, is still missing useful functionality.

--- shell-tunnel.c
+++ shell-tunnel.c
@@ -4,6 +4,9 @@
 #include <libgen.h>
 
 #include <unistd.h>
+#include <termios.h>
+#include <sys/select.h>
+#include <sys/time.h>
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <sys/un.h>
@@ -11,17 +14,20 @@
 #define SERVER_PATH "/tmp/shell-tunnel-socket"
 
 enum {MODE_UNDEF, MODE_DAEMON, MODE_CLIENT};
+enum {CONS_CONFIG, CONS_RESTORE};
 
 static void server_mode(void);
 static void echo(int sockfd);
-static void client_mode(void);
-static void client_test(int sockfd);
+static void client_mode(bool local_echo);
+static int console_proxy(int sockfd, bool local_echo);
+static int byte_interchange(int in_a, int out_a, int in_b, int out_b);
 static void print_usage(const char *name);
 
 int main(int argc, char *argv[])
 {
 	int i;
 	int mode = MODE_UNDEF;
+	bool local_echo = false;
 
 	/* parse command line */
 	for (i = 1; i < argc; ++i) {
@@ -29,6 +35,8 @@ int main(int argc, char *argv[])
 			mode = MODE_DAEMON;
 		else if (!strcmp("--client", argv[i]))
 			mode = MODE_CLIENT;
+		else if (!strcmp("--echo", argv[i]))
+			local_echo = true;
 	}
 
 	switch (mode)
@@ -37,7 +45,7 @@ int main(int argc, char *argv[])
 		server_mode();
 		break;
 	case MODE_CLIENT:
-		client_mode();
+		client_mode(local_echo);
 		break;
 	default:
 		print_usage(basename(argv[0]));
@@ -50,7 +58,7 @@ static void print_usage(const char *name)
 {
 	printf("Usage: \n");
 	printf("%s --daemon \n", name);
-	printf("%s --client \n", name);
+	printf("%s --client [--echo] \n", name);
 	exit(1);
 }
 
@@ -124,7 +132,7 @@ static void echo(int sockfd)
 /*
  * Client side
  */
-void client_mode(void)
+void client_mode(bool local_echo)
 {
 	int err;
 	int sockfd;
@@ -146,7 +154,7 @@ void client_mode(void)
 		goto out_err_2;
 	}
 
-	client_test(sockfd);
+	console_proxy(sockfd, local_echo);
 	shutdown(sockfd, SHUT_RDWR);
 
 out_err_2:
@@ -156,16 +164,100 @@ out_err_1:
 	putchar('\n');
 }
 
-static void client_test(int sockfd)
+static void setup_console(int phase, bool local_echo)
 {
-	int i;
-	const char outbuff[] = "hello world";
-	char inbuff;
+	static struct termios term_state;
+	struct termios new_state;
 
-	for (i = 0; i < strlen(outbuff); ++i) {
-		send(sockfd, &outbuff[i], 1, 0);
-		recv(sockfd, &inbuff, 1, 0);
-		putchar(inbuff);
+	switch (phase)
+	{
+	case CONS_CONFIG:
+		/* switch to raw */
+		tcgetattr(STDIN_FILENO, &term_state);
+		new_state = term_state;
+
+		new_state.c_lflag &= ~(ICANON | (!local_echo ? ECHO : 0));
+
+		tcsetattr(STDIN_FILENO, TCSADRAIN, &new_state);
+		break;
+	case CONS_RESTORE:
+		/* restore */
+		tcsetattr(STDIN_FILENO, TCSADRAIN, &term_state);
+		break;
 	}
+}
+
+static int console_proxy(int sockfd, bool local_echo)
+{
+	int err;
+
+	setup_console(CONS_CONFIG, local_echo);
+	err = byte_interchange(STDIN_FILENO, STDOUT_FILENO, sockfd, sockfd);
+	setup_console(CONS_RESTORE, local_echo);
+	return err;
+}
+
+/*
+ * in_a  --\ /-- in_b
+ *          X
+ * out_a <-/ \-> out_b
+ */
+static int byte_interchange(int in_a, int out_a, int in_b, int out_b)
+{
+	struct timeval tv;
+	fd_set readfds;
+	int len;
+	char buff;
+	int nfds = ((in_a >= in_b) ? in_a : in_b) + 1;
+
+	while (true) {
+		tv.tv_sec = 5;
+		tv.tv_usec = 0;
+		FD_ZERO(&readfds);
+		FD_SET(in_a, &readfds);
+		FD_SET(in_b, &readfds);
+
+		select(nfds, &readfds, NULL, NULL, &tv);
+
+		/* in_a --> out_b */
+		if (FD_ISSET(in_a, &readfds)) {
+			len = read(in_a, &buff, 1);
+			if (len < 0) {
+				perror("could not read file (a)");
+				break;
+			}
+			else if (len == 0) {
+				/* end of file */
+				break;
+			}
+
+			len = write(out_b, &buff, 1);
+			if (len < 1) {
+				perror("could not write file (b)");
+				break;
+			}
+		}
+
+		/* in_b --> out_a */
+		if (FD_ISSET(in_b, &readfds)) {
+			len = read(in_b, &buff, 1);
+			if (len < 0) {
+				perror("could not read file (b)");
+				break;
+			}
+			else if (len == 0) {
+				/* end of file */
+				break;
+			}
+
+			len = write(out_a, &buff, 1);
+			if (len < 1) {
+				perror("could not write file (a)");
+				break;
+			}
+		}
+	}
+
+	return len;
 }
 
